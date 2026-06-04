ea2622ca0af22e13f48dfcd8beed09d69c59914e
Ayushmaan Mishra <ayu.me3008@gmail.com>
2026-06-04 21:19:00 +0530

    Use VITE_API_URL environment variable#2

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_URL 
+  ? import.meta.env.VITE_API_URL + '/api'
+  : 'http://localhost:5000/api'
+
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL + '/api'
+  baseURL: BASE_URL
 })
 
 api.interceptors.request.use((config) => {
@@ -24,4 +28,4 @@ api.interceptors.response.use(
   }
 )
 
-export default api 
+export default api
